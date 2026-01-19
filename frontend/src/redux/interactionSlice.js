443a59060e882d34aa508d626f331a769737ae7d
Hitanshu Sekhar Das <hitanshusekhar480@gmail.com>
2026-01-19 16:29:47 +0530

    Update interactionSlice.js

--- frontend/src/redux/interactionSlice.js
+++ frontend/src/redux/interactionSlice.js
@@ -1,1 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const interactionSlice = createSlice({
+  name: "interaction",
+  initialState: {},
+  reducers: {
+    setInteraction: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { setInteraction } = interactionSlice.actions;
+export default interactionSlice.reducer;
 
