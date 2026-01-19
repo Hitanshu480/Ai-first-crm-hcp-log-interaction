03ec6eeb3b1068e16c3c7ffcc55ac3ceba350287
Hitanshu Sekhar Das <hitanshusekhar480@gmail.com>
2026-01-19 16:30:25 +0530

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,1 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import interactionReducer from "./interactionSlice";
+
+export default configureStore({
+  reducer: {
+    interaction: interactionReducer,
+  },
+});
 
