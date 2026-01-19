f1fd805a90227cf67ddc96a999fb7b6b50b59fde
Hitanshu Sekhar Das <hitanshusekhar480@gmail.com>
2026-01-19 18:52:44 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from agent.graph import build_graph
 from langchain_core.messages import HumanMessage
 
 app = FastAPI()
-agent = build_graph()
 
 class ChatRequest(BaseModel):
     message: str
 
 @app.post("/agent/chat")
 def chat(req: ChatRequest):
-    result = agent.invoke([HumanMessage(content=req.message)])
-    return {"response": result[-1].content}
+    # IMPORT LANGGRAPH ONLY AT RUNTIME
+    from agent.graph import build_graph
+
+    agent = build_graph()  # lazy compile (runtime)
+    result = agent.invoke(req.message)
+
+    return {"response": result}
