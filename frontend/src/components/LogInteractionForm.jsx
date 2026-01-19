284ed16c1960dafafc552f8df819a032cc1b4a32
Hitanshu Sekhar Das <hitanshusekhar480@gmail.com>
2026-01-19 16:31:48 +0530

    Update LogInteractionForm.jsx

--- frontend/src/components/LogInteractionForm.jsx
+++ frontend/src/components/LogInteractionForm.jsx
@@ -1,1 +1,15 @@
+import { useSelector } from "react-redux";
+
+export default function LogInteractionForm() {
+  const data = useSelector((state) => state.interaction);
+
+  return (
+    <div>
+      <input value={data.hcp_name || ""} disabled />
+      <input value={data.topics_discussed || ""} disabled />
+      <input value={data.sentiment || ""} disabled />
+      <textarea value={data.follow_up_actions || ""} disabled />
+    </div>
+  );
+}
 
