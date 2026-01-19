30fdfff29b9201d53430f4602e280cba8cd697e1
Hitanshu Sekhar Das <hitanshusekhar480@gmail.com>
2026-01-19 16:32:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,12 @@
+import LogInteractionForm from "./components/LogInteractionForm";
+import AIAssistantChat from "./components/AIAssistantChat";
+
+export default function App() {
+  return (
+    <div style={{ display: "flex", gap: "20px" }}>
+      <LogInteractionForm />
+      <AIAssistantChat />
+    </div>
+  );
+}
 
